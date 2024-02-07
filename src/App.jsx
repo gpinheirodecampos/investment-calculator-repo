03cfa0002500ac74e4dab9e100cfa612190944c6
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2024-02-07 13:39:18 -0300

    feat: adapting app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import Header from "./components/Header.jsx"
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
-  return (
-    <Header />
-  )
+	const [userInput, setUserInput] = useState({
+		initialInvestment: 10000,
+		annualInvestment: 1200,
+		expectedReturn: 6,
+		duration: 10,
+	});
+
+  const isValid = userInput.duration >= 1;
+
+	function handleChange(inputLabel, newValue) {
+		setUserInput((prevUserInput) => {
+			return {
+				...prevUserInput,
+				[inputLabel]: +newValue,
+			};
+		});
+	}
+
+	return (
+		<>
+			<Header />
+			<UserInput input={userInput} onChange={handleChange} />
+      {isValid && <Results input={userInput} />}
+      {!isValid && <p className="center">Please enter a duration value greater than zero.</p>}
+		</>
+	);
 }
 
-export default App
+export default App;
